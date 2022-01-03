e1ae66a964709d5e711a434b96dec29c97d33e7f
atu4403 <73111778+atu4403@users.noreply.github.com>
2022-01-04 00:10:44 +0900

    👻 fix: バグ修正

--- test.fish
+++ test.fish
@@ -1,16 +1,17 @@
 function fn
     set v $argv[1]
-    git switch test$v
+    git switch dev$v
     echo test$v >test$v.txt
     git add --all
     git commit -m "commit $v"
+    git switch -c release$v
     git pull --rebase origin main
 end
 
-git switch -c test1 main
-git switch --no-track -c test2 main
-git switch -c test3 origin/main
-git switch --no-track -c test4 origin/main
+git switch -c dev1 main
+git switch --no-track -c dev2 main
+git switch -c dev3 origin/main
+git switch --no-track -c dev4 origin/main
 git br -vv
 #   main  487b38f [origin/main] Initial commit
 #   test1 487b38f Initial commit
