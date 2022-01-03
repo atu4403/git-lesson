0e54e5d37e3b42f66dc58a15c933973198d99fb9
atu4403 <73111778+atu4403@users.noreply.github.com>
2022-01-03 21:36:45 +0900

    👻 fix: バグ修正

--- test.fish
+++ test.fish
@@ -8,11 +8,11 @@ function fn
     git pull --rebase origin main
 end
 
-# git switch -c test1 main
-# git switch --no-track -c test2 main
-# git switch -c test3 origin/main
-# git switch --no-track -c test4 origin/main
-# git br -vv
+git switch -c test1 main
+git switch --no-track -c test2 main
+git switch -c test3 origin/main
+git switch --no-track -c test4 origin/main
+git br -vv
 #   main  487b38f [origin/main] Initial commit
 #   test1 487b38f Initial commit
 #   test2 487b38f Initial commit
